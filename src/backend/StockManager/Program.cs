71f14842c35cf9d9b22221f3f31e56993f7e5111
joaov <joaovitorpaulasantos09@gmail.com>
2026-09-07 00:37:47 -0300

    feat: adiciona endpoit de health e configura swagger

---
+++ src/backend/StockManager/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/health", () =>
+{
+    return new {Status = "healthy"};
+});
+
+app.Run();
+
+
